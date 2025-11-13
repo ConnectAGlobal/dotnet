6a6c740120efe673eadf3fea973fc856ef6cb530
Pedro Henrique <ph6704938@gmail.com>
2025-11-13 09:17:10 -0300

    feat: add Profile entity and mapping for user profiles

---
+++ ConnectA.Domain/Entities/Profile.cs
@@ -0,0 +1,29 @@
+﻿namespace ConnectA.Domain.Entities;
+
+public class Profile
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+    public string Biography { get; set; }
+    public string Skills { get; set; }
+    public string Experience { get; set; }
+    public string Objectives { get; set; }
+    public string Location { get; set; }
+    public string Lenguages { get; set; }
+    
+    private Profile() {}
+    
+    public Profile(Guid userId, string biography, string skills, string experience, string objectives, string location, string lenguages)
+    {
+        Id = Guid.NewGuid();
+        UserId = userId;
+        Biography = biography;
+        Skills = skills;
+        Experience = experience;
+        Objectives = objectives;
+        Location = location;
+        Lenguages = lenguages;
+    }
+    
+}
--- ConnectA.Domain/Entities/User.cs
+++ ConnectA.Domain/Entities/User.cs
@@ -14,6 +14,7 @@ public class User
     public UserType Type { get; set; }
     public DateTime CreatedAt { get; private set; }
     public bool Active { get; set; }
+    public Profile Profile { get; set; }
     
     private User() {}
     
