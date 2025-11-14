6f2ffe4d9b0ffe395b957b9b3202523e18219db4
Pedro Henrique <ph6704938@gmail.com>
2025-11-14 11:10:25 -0300

    feat: add DbSet properties and mappings for LearningTrackUser, MentorshipChallenge, MentorshipEvaluation, MentorshipMatch, and TrackStage

--- ConnectA.Infrastructure/Persistence/OracleContext.cs
+++ ConnectA.Infrastructure/Persistence/OracleContext.cs
@@ -9,12 +9,22 @@ public class OracleContext(DbContextOptions<OracleContext> options) : DbContext(
     
     public DbSet<User> Users { get; set; }
     public DbSet<Profile> Profiles { get; set; }
-    public  DbSet<LearningTrack> LearningTracks { get; set; }
+    public DbSet<LearningTrack> LearningTracks { get; set; }
+    public DbSet<LearningTrackUser> LearningTrackUsers { get; set; }
+    public DbSet<MentorshipChallenge> MentorshipChallenges { get; set; }
+    public DbSet<MentorshipEvaluation> MentorshipEvaluations { get; set; }
+    public DbSet<MentorshipMatch> MentorshipMatches { get; set; }
+    public DbSet<TrackStage> TrackStages { get; set; }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new UserMapping());
         modelBuilder.ApplyConfiguration(new ProfileMapping());
         modelBuilder.ApplyConfiguration(new LearningTrackMapping());
+        modelBuilder.ApplyConfiguration(new LearningTrackUserMapping());
+        modelBuilder.ApplyConfiguration(new MentorshipChallengeMapping());
+        modelBuilder.ApplyConfiguration(new MentorshipEvaluationMapping());
+        modelBuilder.ApplyConfiguration(new MentorshipMatchMapping());
+        modelBuilder.ApplyConfiguration(new TrackStageMapping());
     }
 }
