82e4a8ebfae340a09f1235ac7e31150884a58e99
Pedro Henrique <ph6704938@gmail.com>
2025-11-18 23:00:01 -0300

    feat: add MatchController with CreateMatch endpoint for match generation

---
+++ ConnectA.API/Controllers/MatchController.cs
@@ -0,0 +1,23 @@
+﻿using Asp.Versioning;
+using ConnectA.Application.UseCases.Matching;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConnectA.API.Controllers;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/matchs")]
+[Produces("application/json")]
+[ApiVersion(1.0)]
+public class MatchController(
+    GenerateMatchUseCase generateMatchUseCase
+    ) : ControllerBase
+{
+    [HttpPost("match")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateMatch()
+    {
+        await generateMatchUseCase.GenerateMatch();
+        return Ok("Match generation process started.");
+    }
+}
